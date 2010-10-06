35ae3eb8b4a36ab27068662e361ee04a645d6b22
jimmyzimms_cp <jimmyzimms@users.noreply.github.com>
2010-10-06 21:16:10 +0000

    setting CLSCompliant(true) on the assembly setting CA to All Rules

--- Trunk/Source/WS BaseFaults/Properties/AssemblyInfo.cs
+++ Trunk/Source/WS BaseFaults/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -20,6 +21,9 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6e6e3215-9ca3-49cf-83a3-c3afec3a14d2")]
+
+// Marking CLS Compliance
+[assembly: CLSCompliant(true)]
 
 // Version information for an assembly consists of the following four values:
 //
