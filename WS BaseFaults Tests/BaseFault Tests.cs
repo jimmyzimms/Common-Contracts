6029186ea64e9d98be08a78243ed52b87e30148d
jimmyzimms_cp <SND\jimmyzimms_cp>
2010-10-06 19:37:04 +0000

    initial load of WS-BaseFaults code

---
+++ WS BaseFaults Tests/BaseFault Tests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Moles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CommonContracts.WsBaseFaults.Tests
+{
+    [TestClass()]
+    public class BaseFaultTests
+    {
+        /// <summary>
+        /// Moles and Moq dont always work nicely together so this one type is required for the <see cref="ConstructorShouldSetToUtcNow"/> test.
+        /// </summary>
+        private sealed class TestFault : BaseFault
+        {
+            public TestFault() {}
+
+            public TestFault(BaseFault faultCause) : base(faultCause) {}
+        }
+
+        [TestMethod()]
+        [HostType("Moles")]
+        [Description("Confirms that the constructor uses the DateTime.UtcNow value")]
+        public void ConstructorShouldSetToUtcNow()
+        {
+            MDateTime.UtcNowGet = () => new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+            BaseFault target = new TestFault();
+            Assert.AreEqual(new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), target.Timestamp);
+
+            target = new TestFault(target);
+            Assert.AreEqual(new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), target.Timestamp);
+        }
+
+        [TestMethod()]
+        [Description("Confirms that the parameterized constructor uses the supplied DateTime / BaseFault values")]
+        public void ConstructorShouldSetSuppliedParametersoProperties()
+        {
+            var now = DateTime.UtcNow;
+            var mock = new Mock<BaseFault>(now);
+            mock.CallBase = true;
+
+            BaseFault target = mock.Object;
+            Assert.AreEqual(now, target.Timestamp);
+            Assert.IsNull(target.FaultCause);
+
+            mock = new Mock<BaseFault>(target, now);
+            mock.CallBase = true;
+
+            var parentTarget = mock.Object;
+            Assert.AreEqual(now, parentTarget.Timestamp);
+            Assert.IsNotNull(parentTarget.FaultCause);
+            Assert.AreSame(parentTarget.FaultCause, target);
+        }
+
+        [TestMethod()]
+        [Description("Confirms that the parameterized constructor will coerce a non UTC DateTime value")]
+        public void ConstructorShouldSetDateToUtc()
+        {
+            var now = DateTime.Now;
+            Assert.AreEqual(DateTimeKind.Local, now.Kind); // Sanity check
+
+            var mock = new Mock<BaseFault>(now);
+            mock.CallBase = true;
+
+            BaseFault target = mock.Object;
+            Assert.AreEqual(now.ToUniversalTime(), target.Timestamp);
+
+            mock = new Mock<BaseFault>(target, now);
+            mock.CallBase = true;
+
+            var parentTarget = mock.Object;
+            Assert.AreEqual(now.ToUniversalTime(), parentTarget.Timestamp);
+        }
+
+        [TestMethod()]
+        [Description("Confirms that the FaultSource property can be set/get")]
+        public void CanSetFaultCauseProperty()
+        {
+            var now = DateTime.Now;
+            var mock = new Mock<BaseFault>(now);
+            mock.CallBase = true;
+
+            BaseFault target = mock.Object;
+            Assert.IsNull(target.FaultCause); // Sanity check
+
+            mock = new Mock<BaseFault>(now);
+            mock.CallBase = true;
+            BaseFault otherFault = mock.Object;
+
+            target.FaultCause = otherFault;
+            Assert.AreSame(target.FaultCause, otherFault);
+        }
+
+        [TestMethod()]
+        [Description("Confirms that the FaultSource property allows null")]
+        public void CanSetFaultCausePropertyToNull()
+        {
+            var now = DateTime.Now;
+            var mock = new Mock<BaseFault>(now);
+            mock.CallBase = true;
+
+            BaseFault target = mock.Object;
+            Assert.IsNull(target.FaultCause); // Sanity check
+            target.FaultCause = null;
+            Assert.IsNull(target.FaultCause);
+
+            mock = new Mock<BaseFault>(now);
+            mock.CallBase = true;
+            BaseFault otherFault = mock.Object;
+
+            target.FaultCause = otherFault;
+            Assert.IsNotNull(target.FaultCause); // Sanity check
+            target.FaultCause = null;
+            Assert.IsNull(target.FaultCause);
+        }
+    }
+}
---
+++ WS BaseFaults/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WS BaseFaults Common Contracts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jimmy Zimmerman")]
+[assembly: AssemblyProduct("WS BaseFaults Common Contracts")]
+[assembly: AssemblyCopyright("Copyright © Jimmy Zimmerman 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6e6e3215-9ca3-49cf-83a3-c3afec3a14d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
