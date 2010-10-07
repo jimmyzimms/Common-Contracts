28b0ac9a072f35f0b693006ff7c711568f18679a
jimmyzimms_cp <jimmyzimms@users.noreply.github.com>
2010-10-07 15:31:01 +0000

    BaseFaults now serialize and deserialize nested types

---
+++ Trunk/Source/WS BaseFaults Tests/BaseFaultFull Tests.Partials.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Xml.Serialization;
+
+namespace CommonContracts.WsBaseFaults.Tests
+{
+    /// <content>Partials class file.</content>
+    public partial class BaseFaultFullTests
+    {
+        /// <summary>
+        /// Required for use on the <see cref="ConstructorShouldAllowNulls"/> as the constructors are ambiguous with a mock
+        /// Required for the <see cref="Deserialize"/> test.
+        /// Required for the <see cref="NestedFaultsShouldSerialize"/> test.
+        /// </summary>
+        [XmlRoot("BaseFault", Namespace = Constants.WsBaseFaultsNamespace, DataType = Constants.WsBaseFaultsNamespace + ":BaseFaultType")]
+        public sealed class TestFault : BaseFaultFull
+        {
+            public TestFault() { }
+
+            public TestFault(EndpointAddress originator)
+                : base(originator)
+            {
+            }
+
+            public TestFault(IEnumerable<Description> descriptions)
+                : base(descriptions)
+            {
+            }
+
+            public TestFault(ErrorCode errorCode)
+                : base(errorCode)
+            {
+            }
+
+            public TestFault(DateTime utc)
+                : base(utc)
+            {
+            }
+
+            public TestFault(DateTime utc, EndpointAddress originator, ErrorCode errorCode, IEnumerable<Description> descriptions)
+                : base(utc, originator, errorCode, descriptions)
+            {
+            }
+        }
+    }
+}
