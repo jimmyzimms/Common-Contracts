703e16deb843963a32bafa8611acf5f95bf0881d
jimmyzimms_cp <SND\jimmyzimms_cp>
2012-01-26 21:06:46 +0000

    slight edits to the service interface

--- Trunk/Source/WS Eventing/Faults/UnableToRenewFault.cs
+++ Trunk/Source/WS Eventing/Faults/UnableToRenewFault.cs
@@ -66,6 +66,7 @@ namespace CommonContracts.WsEventing.Faults
     /// for local reasons unrelated to the specific request. Usually this is the catch-all fault
     /// to be returned when a critical runtime failure has occured.
     /// </remarks>
+    [XmlSchemaProvider("AcquireSchema")]
     public sealed class UnableToRenewFault : IXmlSerializable
     {
         #region Fields
@@ -105,6 +106,20 @@ namespace CommonContracts.WsEventing.Faults
 
         void IXmlSerializable.WriteXml(XmlWriter writer)
         {
+        }
+
+        #endregion
+
+        #region Schema
+
+        /// <summary>
+        /// Adds an <see cref="XmlSchema"/> instance for this type to the supplied <see cref="XmlSchemaSet"/>.
+        /// </summary>
+        /// <param name="xs">The <see cref="XmlSchemaSet"/> to add an <see cref="XmlSchema"/> to.</param>
+        /// <returns>An <see cref="XmlQualifiedName"/> for the current object.</returns>
+        public static XmlQualifiedName AcquireSchema(XmlSchemaSet xs)
+        {
+            return new XmlQualifiedName("any", "http://www.w3.org/2001/XMLSchema");
         }
 
         #endregion
--- Trunk/Source/WS Eventing/IEventSource.cs
+++ Trunk/Source/WS Eventing/IEventSource.cs
@@ -50,7 +50,6 @@
 #endregion
 
 using System.ServiceModel;
-using CommonContracts.WsEventing.Faults;
 
 namespace CommonContracts.WsEventing
 {
@@ -77,10 +76,10 @@ namespace CommonContracts.WsEventing
         /// <param name="request">The <see cref="SubscribeRequestMessage">request message</see> containing the subscription request details.</param>
         /// <returns>The <see cref="SubscribeResponseMessage">SubscribeResponseMessage</see> containing the subscription details.</returns>
         [OperationContract(Action = Constants.WsEventing.Actions.Subscribe, ReplyAction = Constants.WsEventing.Actions.SubscribeReply)]
-        [FaultContract(typeof(SupportedDeliveryModeFault), Name = "SupportedDeliveryModeFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
-        [FaultContract(typeof(SupportedDialectFault), Name = "SupportedDialectFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
-        [FaultContract(typeof(InvalidExpirationTimeFault), Name = "InvalidExpirationTimeFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
-        [FaultContract(typeof(InvalidMessageFault), Name = "InvalidMessageFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
+        //[FaultContract(typeof(SupportedDeliveryModeFault), Name = "SupportedDeliveryModeFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
+        //[FaultContract(typeof(SupportedDialectFault), Name = "SupportedDialectFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
+        //[FaultContract(typeof(InvalidExpirationTimeFault), Name = "InvalidExpirationTimeFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
+        //[FaultContract(typeof(InvalidMessageFault), Name = "InvalidMessageFault", Action = "http://schemas.xmlsoap.org/ws/2004/08/addressing/fault")]
         [TransactionFlow(TransactionFlowOption.Allowed)]
         SubscribeResponseMessage Subscribe(SubscribeRequestMessage request);
     }
